7653a0ebf445dea7ec179de21b188d8a41cde0a9
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2019-04-21 21:08:15 +0300

    Construirea fragmentului Sign-up (basic logic)

--- app/src/main/java/ordo/azurewebsites/net/ordo/SignUpActivity.java
+++ app/src/main/java/ordo/azurewebsites/net/ordo/SignUpActivity.java
@@ -1,13 +1,14 @@
 package ordo.azurewebsites.net.ordo;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+    protected Fragment createFragment() {
+        return new SignUpFragment();
     }
+
 }
