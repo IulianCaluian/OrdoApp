725038939287c76f22c593b203b36fc44873a876
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2019-04-22 13:24:19 +0300

    Actualizare fragment scan

--- app/src/main/java/ordo/azurewebsites/net/ordo/ScanFragment.java
+++ app/src/main/java/ordo/azurewebsites/net/ordo/ScanFragment.java
@@ -1,18 +1,34 @@
 package ordo.azurewebsites.net.ordo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class ScanFragment extends Fragment {
+    private FloatingActionButton mScanButton;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_scan,container,false);
+
+
+        mScanButton = view.findViewById(R.id.scan_button);
+
+        mScanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), ScanQRActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return view;
     }
 }
