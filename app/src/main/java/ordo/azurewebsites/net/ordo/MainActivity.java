34dd4bc41ae63f7e49f981700c502d0c726c6d36
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2019-04-22 09:23:59 +0300

    Adaugare drawer menu

--- app/src/main/java/ordo/azurewebsites/net/ordo/MainActivity.java
+++ app/src/main/java/ordo/azurewebsites/net/ordo/MainActivity.java
@@ -1,13 +1,38 @@
 package ordo.azurewebsites.net.ordo;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,mToolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 }
