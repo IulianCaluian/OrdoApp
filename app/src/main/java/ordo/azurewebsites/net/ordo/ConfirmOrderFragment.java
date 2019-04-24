e81a271ec9fefdaa7fb99bfe3244e4f3590468ba
Caluian Iulian <iulian.caluian@my.fmi.unibuc.ro>
2019-04-24 04:49:31 +0300

    Adaugare AsyncTask pentru trimitere cerere

--- app/src/main/java/ordo/azurewebsites/net/ordo/ConfirmOrderFragment.java
+++ app/src/main/java/ordo/azurewebsites/net/ordo/ConfirmOrderFragment.java
@@ -2,7 +2,10 @@ package ordo.azurewebsites.net.ordo;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
@@ -66,80 +69,87 @@ public class ConfirmOrderFragment extends DialogFragment {
                         itemOrder.setTitle(message);
                         ItemOrderLib.get(getActivity()).addItemOrder(itemOrder);
 
-
-
-
-                        final ConnectionFactory factory = new ConnectionFactory();
-                        final String QUEUE_NAME = "restaurant_q_" + itm.getRestaurantId();
-
-
-
-                        final JSONObject obj = new JSONObject();
-                        try {
-                            obj.put("order", message);
-                            obj.put("order_id",itemOrder.getId());
-                            obj.put("client","test@test.ro");
-                            obj.put("table", 2);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-
-
-                        factory.setUsername("mxexrkxf");
-                        factory.setPassword("GFppRhHEqe047SjDERyqjrBAFqNBvehV");
-                        // factory.Port = 15672;
-                        factory.setHost("reindeer.rmq.cloudamqp.com");
-                        factory.setVirtualHost("mxexrkxf");
-                        Log.e("ROZ", "avem o incercare " + message + " Pe canalul " + QUEUE_NAME);
-
-                        Thread thread = new Thread() {
-                            @Override
-                            public void run() {
-
-                                Connection connection = null;
-                                try {
-                                    connection = factory.newConnection();
-                                    Channel channel = null;
-                                    try {
-
-
-                                        channel = connection.createChannel();
-                                        channel.queueDeclare(QUEUE_NAME, true, false, false, null);
-                                        channel.basicPublish("", QUEUE_NAME, null, obj.toString().getBytes("UTF-8"));
-                                        Log.e("ROZ", "avem o publicare " + message + " Pe canalul " + QUEUE_NAME);
-                                    }finally {
-                                        try {
-                                            if(channel!=null && channel.isOpen())
-                                                channel.close();
-                                        } catch (IOException e1) {
-                                            e1.printStackTrace();
-                                        } catch (TimeoutException e1) {
-                                            e1.printStackTrace();
-                                        }
-                                    }
-                                } catch (UnsupportedEncodingException e) {
-                                    e.printStackTrace();
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                } catch (TimeoutException e) {
-                                    e.printStackTrace();
-                                } finally{
-                                    try {
-                                        if(connection!=null && connection.isOpen())
-                                            connection.close();
-                                    } catch (IOException e1) {
-                                        e1.printStackTrace();
-                                    }
-                                }
-                            }
-                        };
-                        thread.start();
+                        new SendRequestTask().execute(itemOrder);
 
                     }
                 })
                 .setNegativeButton(android.R.string.cancel,null)
                 .create();
+    }
+
+    private class SendRequestTask extends AsyncTask<ItemOrder, Void, Void>{
+
+        @Override
+        protected Void doInBackground(ItemOrder... itemOrders) {
+            if(itemOrders == null || itemOrders.length ==0) return null;
+            ItemOrder itemOrder = itemOrders[0];
+
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            int restaurant_id = sharedPref.getInt(getString(R.string.save_restaurant_id),1);
+
+            String message = itemOrder.getTitle();
+            ConnectionFactory factory = new ConnectionFactory();
+            String QUEUE_NAME = "restaurant_q_" + restaurant_id;
+
+
+
+            final JSONObject obj = new JSONObject();
+            try {
+                obj.put("order", message);
+                obj.put("order_id",itemOrder.getId());
+                obj.put("client","test@test.ro");
+                obj.put("table", 2);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+
+
+            factory.setUsername("mxexrkxf");
+            factory.setPassword("GFppRhHEqe047SjDERyqjrBAFqNBvehV");
+            // factory.Port = 15672;
+            factory.setHost("reindeer.rmq.cloudamqp.com");
+            factory.setVirtualHost("mxexrkxf");
+            Log.e("ROZ", "avem o incercare " + message + " Pe canalul " + QUEUE_NAME);
+
+            Connection connection = null;
+            try {
+                connection = factory.newConnection();
+                Channel channel = null;
+                try {
+
+
+                    channel = connection.createChannel();
+                    channel.queueDeclare(QUEUE_NAME, true, false, false, null);
+                    channel.basicPublish("", QUEUE_NAME, null, obj.toString().getBytes("UTF-8"));
+                    Log.e("ROZ", "avem o publicare " + message + " Pe canalul " + QUEUE_NAME);
+                }finally {
+                    try {
+                        if(channel!=null && channel.isOpen())
+                            channel.close();
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    } catch (TimeoutException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            } finally{
+                try {
+                    if(connection!=null && connection.isOpen())
+                        connection.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            return null;
+        }
     }
 }
